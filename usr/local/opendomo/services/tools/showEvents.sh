22503dc5d35b6e795dc77f0d40501119f7dfbf27
opalenzuela <opalenzuela@gmail.com>
2014-03-11 14:37:34 +0100

    improving script format

--- usr/local/opendomo/services/tools/showEvents.sh
+++ usr/local/opendomo/services/tools/showEvents.sh
@@ -7,7 +7,7 @@ LOGFILE="/var/opendomo/log/events.log"
 echo "list:showEvents.sh"
 if test -f $LOGFILE
 then
-	tail -n100 $LOGFILE |sed 's/^/\t/' |sed 's/ /-/g' |sed 's/ /\t/' | sed 's/$/\t event/'
+	tail -n100 $LOGFILE |sed 's/^/\t-/' |sed 's/ /-/g' |sed 's/ /\t/' | sed 's/$/\t event/'
 else
 	echo "#ERROR Invalid file $LOGFILE"
 fi
