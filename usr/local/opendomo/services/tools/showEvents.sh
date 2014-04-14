a76ee8036ee91364447d822d15f00ae9c57ef303
opalenzuela <opalenzuela@gmail.com>
2014-04-14 15:24:42 +0200

    fixing format

--- usr/local/opendomo/services/tools/showEvents.sh
+++ usr/local/opendomo/services/tools/showEvents.sh
@@ -3,11 +3,13 @@
 #type:local
 #package:odevents
 
+# Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
+
 LOGFILE="/var/opendomo/log/events.log"
 echo "list:showEvents.sh"
 if test -f $LOGFILE
 then
-	tail -n100 $LOGFILE |sed 's/^/\t-/' |sed 's/ /-/g' |sed 's/ /\t/' | sed 's/$/\t event/'
+	tail -n100 $LOGFILE |sed 's/^/\t-/' | sed 's/ /\t/' | sed 's/$/\t event/'
 else
 	echo "#ERROR Invalid file $LOGFILE"
 fi
