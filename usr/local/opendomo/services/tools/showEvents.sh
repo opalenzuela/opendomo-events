fb7a30d6cfca9094404f85a62a8185c25cf913d3
opalenzuela <opalenzuela@gmail.com>
2014-07-30 14:19:36 +0200

    fixing sed expression

--- usr/local/opendomo/services/tools/showEvents.sh
+++ usr/local/opendomo/services/tools/showEvents.sh
@@ -6,7 +6,7 @@
 # Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
 
 LOGFILE="/var/opendomo/log/events.log"
-SEDFILTER='s/^\([0-9:;]*\)\t\([a-z]*\).\([a-z]*\)\t\([^/]*\)/\t-\1-\2.\3\t\4\t\3\t\1/g'
+SEDFILTER='s/^/\t/g'
 
 if ! test -r $LOGFILE; then
         echo "#ERR: Access denied!"
@@ -18,13 +18,13 @@ if test -z "$1" || test "$1" = "filter"; then
 		echo "list:`basename $0`"
 		case $2 in
 				warn)
-						FILTER=' -e .warn -e .error -e .crit '
+						FILTER=' -e warn -e error -e crit '
 				;;
 				error)
-						FILTER=' -e .error -e .crit '
+						FILTER=' -e error -e crit '
 				;;
 				*)
-						FILTER=' -v .debug '
+						FILTER=' -v debug '
 				;;
 		esac
 		sort -r $LOGFILE | grep $FILTER | sed $SEDFILTER | head -n 100  
@@ -32,7 +32,7 @@ if test -z "$1" || test "$1" = "filter"; then
 		echo "#>Filter"
 		echo "form:`basename $0`"
 		echo "	type	type	hidden 	filter"
-		echo "	filter  Filter	list[all,warn,error]	$1"
+		echo "	filter	Filter	list[all,warn,error]	$1"
 		echo "actions:"
 		echo "	showEvents.sh	Apply filter"
 else
