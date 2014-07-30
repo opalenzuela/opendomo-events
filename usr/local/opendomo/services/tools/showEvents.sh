18c6e43b4c7cfb6f41f480b9b3985955d1b9638c
opalenzuela <opalenzuela@gmail.com>
2014-07-30 14:14:07 +0200

    Adding filters

--- usr/local/opendomo/services/tools/showEvents.sh
+++ usr/local/opendomo/services/tools/showEvents.sh
@@ -6,12 +6,41 @@
 # Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
 
 LOGFILE="/var/opendomo/log/events.log"
-echo "#> Events"
-echo "list:showEvents.sh"
-if test -f $LOGFILE
-then
-	tail -n100 $LOGFILE |sed 's/^/\t-/' | sed 's/ /\t/' | sed 's/$/\t event/'
+SEDFILTER='s/^\([0-9:;]*\)\t\([a-z]*\).\([a-z]*\)\t\([^/]*\)/\t-\1-\2.\3\t\4\t\3\t\1/g'
+
+if ! test -r $LOGFILE; then
+        echo "#ERR: Access denied!"
+        exit 1
+fi
+
+if test -z "$1" || test "$1" = "filter"; then
+		echo "#> Events"
+		echo "list:`basename $0`"
+		case $2 in
+				warn)
+						FILTER=' -e .warn -e .error -e .crit '
+				;;
+				error)
+						FILTER=' -e .error -e .crit '
+				;;
+				*)
+						FILTER=' -v .debug '
+				;;
+		esac
+		sort -r $LOGFILE | grep $FILTER | sed $SEDFILTER | head -n 100  
+		echo
+		echo "#>Filter"
+		echo "form:`basename $0`"
+		echo "	type	type	hidden 	filter"
+		echo "	filter  Filter	list[all,warn,error]	$1"
+		echo "actions:"
+		echo "	showEvents.sh	Apply filter"
 else
-	echo "#ERROR Invalid file $LOGFILE"
+		echo "# Event information"
+		TIME=`echo $1 | cut -f1 -d-`
+		TYPE=`echo $1 | cut -f2 -d-`
+		grep $TIME $LOGFILE | grep $TYPE | sed 's/^/# /'
+		echo "actions:"
+		echo "	showEvents.sh	Back to list"
 fi
 echo
