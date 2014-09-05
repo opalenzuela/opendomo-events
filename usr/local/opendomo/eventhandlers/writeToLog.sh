dc676cd66a6a6ae3899712d36a981f8f44afcbf3
opalenzuela <opalenzuela@gmail.com>
2014-09-05 12:43:52 +0200

    Formatting already in log

--- usr/local/opendomo/eventhandlers/writeToLog.sh
+++ usr/local/opendomo/eventhandlers/writeToLog.sh
@@ -2,4 +2,4 @@
 #desc:Write to log file
 USER=`whoami`
 DATE=`date +"%Y-%m-%d.%H:%M:%S.%N"`
-echo $DATE $USER $1 $2 $3 $4 $5 >> /var/opendomo/log/events.log
+echo $DATE	$4	$USER $1-$2 $3 	$5 >> /var/opendomo/log/events.log
