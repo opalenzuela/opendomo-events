1a2eee0ea4f7aaf785153faf55eceeec91dd7f2e
opalenzuela <opalenzuela@gmail.com>
2014-09-05 12:31:35 +0200

    Wrong character

--- usr/local/opendomo/eventhandlers/writeToLog.sh
+++ usr/local/opendomo/eventhandlers/writeToLog.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #desc:Write to log file
 USER=`whoami`
-DATE=`date +"%Y-%m-%d.%h:%M:%S.%N"`
+DATE=`date +"%Y-%m-%d.%H:%M:%S.%N"`
 echo $DATE $USER $1 $2 $3 $4 $5 >> /var/opendomo/log/events.log
