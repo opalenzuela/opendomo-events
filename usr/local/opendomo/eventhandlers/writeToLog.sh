f534567b870ee9f23b9d2d0bc4b6fa521f1b9c73
opalenzuela <opalenzuela@gmail.com>
2014-09-05 12:30:29 +0200

    Improving details written to log

--- usr/local/opendomo/eventhandlers/writeToLog.sh
+++ usr/local/opendomo/eventhandlers/writeToLog.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #desc:Write to log file
-echo $1 $2 $3 $4 $5 >> /var/opendomo/log/events.log
+USER=`whoami`
+DATE=`date +"%Y-%m-%d.%h:%M:%S.%N"`
+echo $DATE $USER $1 $2 $3 $4 $5 >> /var/opendomo/log/events.log
