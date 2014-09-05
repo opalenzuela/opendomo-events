9f168270f5426edeea8a4a3f2041c13af223ad86
opalenzuela <opalenzuela@gmail.com>
2014-09-05 12:48:57 +0200

    reordering

--- usr/local/opendomo/eventhandlers/writeToLog.sh
+++ usr/local/opendomo/eventhandlers/writeToLog.sh
@@ -2,4 +2,4 @@
 #desc:Write to log file
 USER=`whoami`
 DATE=`date +"%Y-%m-%d.%H:%M:%S.%N"`
-echo "$DATE	$4	$USER $1-$2 $3 	$5" >> /var/opendomo/log/events.log
+echo "$DATE	$3	$USER $1-$2 $4	$5" >> /var/opendomo/log/events.log
