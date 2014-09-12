a74f6147c197971cb79f91ec7a9d1897964b637b
opalenzuela <opalenzuela@gmail.com>
2014-09-12 13:19:30 +0200

    order failing

--- usr/local/opendomo/eventhandlers/writeToLog.sh
+++ usr/local/opendomo/eventhandlers/writeToLog.sh
@@ -2,4 +2,4 @@
 #desc:Write to log file
 USER=`whoami`
 DATE=`date +"%Y-%m-%d.%H:%M:%S.%N"`
-echo "$DATE	$3	$USER $1-$2 $4	$5" >> /var/opendomo/log/events.log
+echo "$DATE	$3	$USER $2-$1 $4	$5" >> /var/opendomo/log/events.log
