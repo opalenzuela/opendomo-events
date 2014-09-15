5d0668cb56ded2adde79ea2fc66acef3834d649d
opalenzuela <opalenzuela@gmail.com>
2014-09-15 09:47:38 +0200

    Rotating logs

--- usr/local/opendomo/eventhandlers/writeToLog.sh
+++ usr/local/opendomo/eventhandlers/writeToLog.sh
@@ -3,3 +3,10 @@
 USER=`whoami`
 DATE=`date +"%Y-%m-%d.%H:%M:%S.%N"`
 echo "$DATE	$3	$USER $2-$1 $4	$5" >> /var/opendomo/log/events.log
+LOGSIZE=`wc -c /var/opendomo/log/events.log | cut -f1 -d' '`
+if test $LOGSIZE -gt 1000000
+then
+	DATE=`date +"%Y%m%d"`
+	mv /var/opendomo/log/events.log /var/opendomo/log/events.$DATE.log
+	gzip /var/opendomo/log/events.$DATE.log
+fi
