87966e35f73b1b0888c9e4b554815096c9435e85
opalenzuela <opalenzuela@gmail.com>
2014-09-15 16:12:15 +0200

    wrong order

--- usr/local/opendomo/eventhandlers/refreshToken.sh
+++ usr/local/opendomo/eventhandlers/refreshToken.sh
@@ -2,8 +2,10 @@
 #desc:Update Cloud token
 #package:odevents
 
-uid=`cat  $UIDFILE `
+# Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
+
 UIDFILE="/etc/opendomo/uid"
+uid=`cat  $UIDFILE `
 source /etc/opendomo/udata/admin.info
 URL="http://cloud.opendomo.com/activate/index.php?UID=$uid&MAIL=$EMAIL"
 wget -q -O /var/opendomo/tmp/refreshToken.tmp $URL 2>/dev/null
--- usr/local/opendomo/eventhandlers/writeToLog.sh
+++ usr/local/opendomo/eventhandlers/writeToLog.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 #desc:Write to log file
+
+# Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
+
 USER=`whoami`
 DATE=`date +"%Y-%m-%d.%H:%M:%S.%N"`
 echo "$DATE	$3	$USER $2-$1 $4	$5" >> /var/opendomo/log/events.log
