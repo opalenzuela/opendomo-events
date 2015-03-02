56498430f6b9c20a610e4361d7d69cfa595b302b
opalenzuela <opalenzuela@gmail.com>
2015-03-02 10:13:42 +0100

    Preparing for #128

--- var/www/scripts/common/checkForPrompt.js
+++ var/www/scripts/common/checkForPrompt.js
@@ -1,12 +1,2 @@
+var promptworker = new Worker('/scripts/prompt.js');
 
-setInterval(function () {
-	try {
-		var prompting = loadJSON("/data/prompts.json");
-		if (prompting.length>0) {
-			$("#cat-config").addClass("prompting");
-			$("#cat-config a").prop("href","");
-		}
-	} catch (e) {
-		// Silently quit
-	}
-}, 10000);
---
+++ var/www/scripts/prompt.js
@@ -0,0 +1,11 @@
+setInterval(function () {
+	try {
+		var prompting = loadJSON("/data/prompts.json");
+		if (prompting.length>0) {
+			$("#cat-config").addClass("prompting");
+			$("#cat-config a").prop("href","");
+		}
+	} catch (e) {
+		// Silently quit
+	}
+}, 10000);
