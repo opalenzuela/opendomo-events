1535e5e54b585a21633f324935cd5a9dfcb97dcf
opalenzuela <opalenzuela@gmail.com>
2015-05-14 10:34:36 +0200

    Abandoning web workers

--- var/www/scripts/common/checkForPrompt.js
+++ var/www/scripts/common/checkForPrompt.js
@@ -1,6 +1,26 @@
-var promptworker = new Worker('/scripts/prompt.js');
+/*var promptworker = new Worker('/scripts/prompt.js');
 promptworker.addEventListener('message', function(e) {
   console.log('Prompt worker said: ', e.data);
 }, false);
 promptworker.postMessage('start'); 
+*/
 
+function checkSystemStatus() {
+	try {
+		var prompting = loadJSON("/data/prompts.json");
+		if (prompting.length>0) {
+			self.postMessage("data");
+		}
+		var s = loadJSON("/data/status.json");
+		if (s.status == "busy") {
+			self.postMessage("busy");
+		}
+	} catch (e) {
+		console.log(e.message);
+		// Silently quit
+	}	
+}
+
+setInterval(function () {
+	
+}, 5000);
